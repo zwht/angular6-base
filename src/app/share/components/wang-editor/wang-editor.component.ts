aa6d497340faef6604c1d6066d889dfa8f4f8567
zhaowei <1512763623@qq.com>
2018-10-26 18:38:13 +0800

    bug

--- src/app/share/components/wang-editor/wang-editor.component.ts
+++ src/app/share/components/wang-editor/wang-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, ViewChild, ElementRef, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 declare var require: any;
-const E = require('wangeditor');
+const E = require('../../../../assets/libs/wangeditor');
 @Component({
   selector: 'app-wang-editor',
   templateUrl: './wang-editor.component.html',
